2e72856e632940e041d464cab96442915fd24701
Randy Cisneros <rlcisneros90@gmail.com>
2023-03-01 14:28:52 -0700

    Add basic structure for player class

---
+++ player.js
@@ -0,0 +1,9 @@
+class player {
+    constructor(playerOne, playerTwo, token) {
+        this.id = token;
+        this.playerOne = playerOne
+        this.playerTwo = playerTwo
+        this.wins = 0
+        this.increaseWins = 0
+    }
+}
