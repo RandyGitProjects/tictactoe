0b5f0bd302d995781694f1110bbd2fcd8383b2e9
Randy Cisneros <rlcisneros90@gmail.com>
2023-03-03 12:53:26 -0700

    Add functionality to the dom by switching emojis between players turn

--- main.js
+++ main.js
@@ -11,22 +11,23 @@ var gridBoard = document.querySelector(".game-board")
 gridBoard.addEventListener('click', function(event) {
     if (event.target.matches('.grid-item')) {
       turnGridArray()
-      tokenText()
-      checkGridItem()
+      toeknSwitch()
       console.log(`Clicked grid item with id ${event.target.id}`);
     }
   });
 
-  function checkGridItem() {
-    if (event.target.id) {
-      game.switchPlayer()     
+  // This function calls the game class currentPlayer and make sure it's player one.
+  // after it rings true we update the DOM using innerText and using the player one token
+  // If currentPlayer does not = player one we update the DOM using innerText using the player two token
+  // After the if statement is finished we switch players using the game.switchPlayer function
+  function tokenSwitch() {
+    if (game.currentPlayer === game.playerOne) {
+      event.target.innerText = game.playerOne.token
+    }else{
+      event.target.innerText = game.playerTwo.token
     }
+    game.switchPlayer()
   }
-
-  
-  function tokenText() {
-  event.target.innerText = game.playerOne.token
-}
 
 function turnGridArray() {
   var array = Object.entries(gridArray)
